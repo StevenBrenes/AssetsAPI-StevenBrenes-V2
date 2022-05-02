8108ade30dacf0204fc4fe698742a7f8b6da14fc
Steven Brenes <sbgdp@outlook.com>
2022-05-02 15:28:18 -0600

    Agregar archivos de proyecto.

---
+++ AssetsAPI{StevenBrenes}V2/Models/Asset.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssetsAPI_StevenBrenes_V2.Models
+{
+    public partial class Asset
+    {
+        public int IdAsset { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+        public int Cost { get; set; }
+    }
+}
---
+++ AssetsAPI{StevenBrenes}V2/Models/AssetsContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AssetsAPI_StevenBrenes_V2.Models
+{
+    public partial class AssetsContext : DbContext
+    {
+        public AssetsContext()
+        {
+        }
+
+        public AssetsContext(DbContextOptions<AssetsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Asset> Assets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("SERVER=192.168.0.13; DATABASE=Assets; User Id=sa; Password=HacHxnZ6");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Asset>(entity =>
+            {
+                entity.HasKey(e => e.IdAsset)
+                    .HasName("PK__assets__F59A5836CF6D8BC7");
+
+                entity.ToTable("assets");
+
+                entity.Property(e => e.Area)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
